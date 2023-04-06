b4f1048da51d2cc9adeb09404ca4b255a9b9922e
Vlad <UladCherap@yandex.by>
2023-04-06 12:28:07 +0300

    Add Entity for Moderation.java.

--- src/main/java/by/easycar/model/Moderation.java
+++ src/main/java/by/easycar/model/Moderation.java
@@ -1,7 +1,27 @@
 package by.easycar.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.hibernate.annotations.Immutable;
+
+@NoArgsConstructor
+@EqualsAndHashCode
+@Getter
+@ToString
+
+@Entity
+@Immutable
+@Table(name = "moderations")
 public class Moderation {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "mo_sequence")
+    @SequenceGenerator(catalog = "sequences", name = "mo_sequence", sequenceName = "moderation_sequence_id", initialValue = 1, allocationSize = 1)
     private Long id;
+    @Column(name = "mo_message")
     private String message;
+    @Transient
     private Admin admin;
 }
