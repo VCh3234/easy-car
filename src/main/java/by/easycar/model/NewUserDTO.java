ecf2f110e656942909925ee2897166e3833c6e35
Vlad <UladCherap@yandex.by>
2023-04-10 13:31:57 +0300

    Changed generationType for id.

--- src/main/java/by/easycar/model/NewUserDTO.java
+++ src/main/java/by/easycar/model/NewUserDTO.java
@@ -1,26 +1,32 @@
 package by.easycar.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
+
+@NoArgsConstructor
+@EqualsAndHashCode
+@Setter
+@Getter
+@ToString
 
 @Entity
 @Table(name = "users")
-@Data
 public class NewUserDTO {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "u_sequence")
+    @SequenceGenerator(catalog = "sequences", name = "u_sequence", sequenceName = "u_sequence_id", initialValue = 1, allocationSize = 1)
+    @Column(name = "u_id")
     private Long id;
 
-    @Column(name = "user_name")
+    @Column(name = "u_name")
     private String name;
 
-    @Column(name = "email")
+    @Column(name = "u_email")
     private String email;
 
-    @Column(name = "password")
-    private String pass;
+    @Column(name = "u_password")
+    private String password;
 
-    @Column(name = "phone_number")
-    private String phoneNumber;
+    @Column(name = "u_phone")
+    private String phoneNumber; //TODO: embeddable class
 }
