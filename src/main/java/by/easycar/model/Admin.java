983b29a460a80829c63d623492bbba618a282e36
Vlad <UladCherap@yandex.by>
2023-04-10 13:28:30 +0300

    Made Entity for Admin.

--- src/main/java/by/easycar/model/Admin.java
+++ src/main/java/by/easycar/model/Admin.java
@@ -1,8 +1,29 @@
 package by.easycar.model;
 
-import jakarta.persistence.criteria.CriteriaBuilder;
+import by.easycar.model.security.Role;
+import jakarta.persistence.*;
+import lombok.*;
 
+@NoArgsConstructor
+@EqualsAndHashCode
+@Setter
+@Getter
+@ToString
+
+@Entity
+@Table(name = "admins")
 public class Admin {
-    private Integer id;
+
+    private final static Role ROLE = Role.ROLE_ADMIN;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "adm_sequence")
+    @SequenceGenerator(catalog = "sequences", name = "adm_sequence", sequenceName = "admins_sequence_id", initialValue = 1, allocationSize = 1)
+    @Column(name = "adm_id")
+    private Long id;
+    @Column(name = "adm_name")
     private String name;
+
+    @Column(name = "adm_status")
+    private String status;
 }
