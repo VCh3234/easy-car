2e7dbdc4ac52a3cd54437014e3aef19971f1b1d1
Vlad <UladCherap@yandex.by>
2023-04-10 13:31:02 +0300

    Added @Column for id field, creationDate field

--- src/main/java/by/easycar/model/Moderation.java
+++ src/main/java/by/easycar/model/Moderation.java
@@ -5,7 +5,10 @@ import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.ToString;
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Immutable;
+
+import java.time.LocalDateTime;
 
 @NoArgsConstructor
 @EqualsAndHashCode
@@ -15,13 +18,24 @@ import org.hibernate.annotations.Immutable;
 @Entity
 @Immutable
 @Table(name = "moderations")
+@Access(AccessType.FIELD) //Default - added for demonstration
 public class Moderation {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "mo_sequence")
-    @SequenceGenerator(catalog = "sequences", name = "mo_sequence", sequenceName = "moderation_sequence_id", initialValue = 1, allocationSize = 1)
+    @SequenceGenerator(catalog = "sequences", name = "mo_sequence", sequenceName = "moderations_sequence_id", initialValue = 1, allocationSize = 1)
+    @Column(name = "mo_id")
     private Long id;
-    @Column(name = "mo_message")
+
+    @Column(name = "mo_creation_date", nullable = false)
+    @CreationTimestamp
+    private LocalDateTime creationDate;
+    @Column(name = "mo_message", nullable = false)
     private String message;
     @Transient
     private Admin admin;
+
+    public Moderation(String message, Admin admin) {
+        this.message = message;
+        this.admin = admin;
+    }
 }
